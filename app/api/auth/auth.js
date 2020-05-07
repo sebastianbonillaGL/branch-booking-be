bf658fd817401e285714ec616c4a5f28ca760845
Sebastian Bonilla <sebastian.bonilla@udr.com>
2020-05-07 10:56:46 -0500

    complete signUp process

--- app/api/auth/auth.js
+++ app/api/auth/auth.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('./../../config/config');
 
 router.post('/login', function (req, res, next) {
-    console.log(req.body);
     passport.authenticate('login', function (err, user, info) {
         if (err) return next(err);
 
         req.login(user, { session: false }, function (error) {
             if (error) return next(error);
 
-            res.json({ message: "login successful" });
+            console.log(user);
+            let token = signToken(user._id);
+            res.json({ token: token });
         });
     })(req, res, next);
 })
+
+router.get('/profile', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    console.log('profile endpoint');
+    res.json(req.user);
+})
+
+let signToken = function (id) {
+    return jwt.sign(
+        { _id: id },
+        config.secrets.jwt
+    )
+}
 
 module.exports = router;
--- app/api/auth/passportSetup.js
+++ app/api/auth/passportSetup.js
@@ -5,12 +5,14 @@ const Oauth2Client = require('google-auth-library').OAuth2Client
 const client = new Oauth2Client(config.google.clientID);
 const jwtStrategy = require('passport-jwt').Strategy;
 const { ExtractJwt } = require('passport-jwt');
-
+const User = require('./user');
 
 passport.use('login', new localStrategy({
+    usernameField: 'username',
     passwordField: 'idToken'
 }, function (username, password, done) {
-    verifyToken(password)
+    verifyToken(username, password)
+        .then(checkUser)
         .then(function (user) {
             return done(null, user);
         })
@@ -23,27 +25,62 @@ passport.use(new jwtStrategy({
     secretOrKey: config.secrets.jwt,
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 }, function (token, done) {
-    done(null, token.user);
+    getFreshUser(token._id)
+    .then(function(user){
+        done(null, user);
+    })
+    .catch(function(reason){
+        done(reason);
+    })
 }));
 
-let verifyToken = function (token) {
+let verifyToken = function (username, token) {
     return new Promise(function (resolve, reject) {
         client.verifyIdToken({
             idToken: token,
             audience: config.google.clientID
+        }).then((ticket) => {
+            let payload = ticket.getPayload();
+            if (payload.hd === "gorillalogic.com") {
+                return resolve({
+                    name: payload.name,
+                    email: payload.email
+                });
+            } else {
+                return reject(Error('You are not allowed to login'));
+            }
+        }, (reason) => {
+            return reject(reason);
         })
-            .then((ticket) => {
-                let payload = ticket.getPayload();
-                if (payload.hd === "gorillalogic.com") {
-                    return resolve({
-                        name: payload.name,
-                        email: payload.email
-                    });
-                } else {
-                    return reject(Error('You are not allowed to login'));
-                }
-            }, (reason) => {
-                return reject(reason);
-            })
     });
 }
+
+let checkUser = function (user) {
+    return new Promise(function (resolve, reject) {
+        User.findOne({ email: user.email }, function (err, currentUser) {
+            if (err) { return reject(err); }
+
+            if (currentUser) {
+                return resolve(currentUser);
+            } else {
+                new User(user).save()
+                    .then(function (newUser) {
+                        return resolve(newUser);
+                    });
+            }
+        });
+    });
+}
+
+let getFreshUser = function (id) {
+    return new Promise(function (resolve, reject) {
+        User.findById(id, function (err, currentUser) {
+            if (err) { return reject(err); }
+
+            if (!currentUser) { return reject(Error('No user found')); }
+
+            return resolve(currentUser);
+        })
+    })
+}
+
