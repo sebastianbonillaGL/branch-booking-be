44ce76ade25b015653ebf737503fe39395e8ecea
Sebastian Bonilla <sebastian.bonilla@udr.com>
2020-05-07 16:38:29 -0500

    secure branch routes

--- app/api/branch/branchRoutes.js
+++ app/api/branch/branchRoutes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const controller = require('./branchController');
+const passport = require('passport');
+
+router.use(passport.authenticate('jwt', {session:false}));
 
 router.route('/')
     .get(controller.get)
