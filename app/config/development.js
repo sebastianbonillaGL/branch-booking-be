0ac7d55494673fe9c8592d06233644f8867e07e6
Sebastian Bonilla <sebastian.bonilla@udr.com>
2020-05-07 17:48:24 -0500

    adjust environments

--- app/config/development.js
+++ app/config/development.js
@@ -1,5 +1,4 @@
 module.exports = {
   clientID: '535761997100-ifdnm3d069nqri3nhdnt9jpr4agr73eu.apps.googleusercontent.com',
-  clientSecret: 'zjzMG7TNjMcvb31z0vjjxmvH',
   MONGODB_URI: 'mongodb://localhost/branch'
 };
---
+++ app/config/production.js
@@ -0,0 +1,4 @@
+module.exports = {
+  clientID: process.env.clientID,
+  MONGODB_URI: process.env.MONGODB_URI
+};
